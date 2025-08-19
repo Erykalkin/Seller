51d02fde89e176c08ba252982dd608c655d12848
Erykalkin <60609438+Erykalkin@users.noreply.github.com>
2025-08-20 02:02:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import asyncio
 from pyrogram import idle
 from bot import bot, periodic_greeting, group_parser
 
-os.environ["http_proxy"] = os.getenv("HTTP_PROXY", "socks5h://127.0.0.1:1082")
-os.environ["https_proxy"] = os.getenv("HTTPS_PROXY", "socks5h://127.0.0.1:1082")
-
 async def main():
     await bot.start()
     print("БОТ ЗАПУЩЕН")
